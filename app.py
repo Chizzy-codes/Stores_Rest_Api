e5c01db6da85ef1611cbf7a95fd0431b11f0a3f4
Chizzy-codes <58339707+Chizzy-codes@users.noreply.github.com>
2020-02-06 18:46:58 +0100

    Fixed not importing db when running the app

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ app.secret_key = 'chizzy'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(ItemList, '/items')
